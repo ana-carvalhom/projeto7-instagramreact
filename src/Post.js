ad418c3e871af08221754cbb1830890d5cf246a7
Ana Carvalho <anapaulacmoura@gmail.com>
2022-11-20 19:19:42 -0300

    adding likes counter functionality

--- src/Post.js
+++ src/Post.js
@@ -5,16 +5,28 @@ export default function Post(props){
     
     const [colorLike, setLike] = React.useState("initial");
     const [nameIcon, setNameIcon] = React.useState("heart-outline");
+    const [savePost, setSavePost] = React.useState("bookmark-outline");
+    const [likes, setLikes] = React.useState(props.likes);
 
     function likePost(){
             if(colorLike === 'initial'){
                 setNameIcon("heart");
                 setLike("red");
+                setLikes(props.likes + 1);
             } else {
                 setNameIcon("heart-outline");
                 setLike("initial")
+                setLikes(props.likes)
             }
         }
+
+    function savedPost(){
+        if (savePost === 'bookmark-outline'){
+            setSavePost("bookmark");
+        } else {
+            setSavePost("bookmark-outline")
+        }
+    }
 
     return(
         <div className="post">
@@ -40,14 +52,14 @@ export default function Post(props){
                     <ion-icon name="paper-plane-outline"></ion-icon>
                 </div>
                 <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
+                    <ion-icon name={savePost} onClick={savedPost}></ion-icon>
                 </div>
             </div>
 
             <div className="curtidas">
                 <img src={props.likedImage} />
                 <div className="texto">
-                    Curtido por <strong>{props.likedBy}</strong> e <strong>outras {props.likes} pessoas</strong>
+                    Curtido por <strong>{props.likedBy}</strong> e <strong>outras {likes} pessoas</strong>
                 </div>
             </div>
         </div>
