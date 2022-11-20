4c9c947f99ab0d8d21ff3761d4082206ab73964c
Ana Carvalho <anapaulacmoura@gmail.com>
2022-11-19 22:58:50 -0300

    creating Sugestao component with map

--- src/Sugestoes.js
+++ src/Sugestoes.js
@@ -1,70 +1,46 @@
+import Sugestao from "./Sugestao"
+
 export default function Sugestoes (){
+    const sugestoes =[
+        {
+            image:"assets/img/bad.vibes.memes.svg",
+            userName:"bad.vibes.memes",
+            status:"Segue você",
+        },
+        {
+            image:"assets/img/chibirdart.svg",
+            userName:"chibirdart",
+            status:"Segue você",
+        },
+        {
+            image:"assets/img/razoesparaacreditar.svg",
+            userName:"razoesparaacreditar",
+            status:"Novo no Instagram",
+        },
+        {
+            image:"assets/img/adorable_animals.svg",
+            userName:"adorable_animals",
+            status:"Segue você",
+        },
+        {
+            image:"assets/img/smallcutecats.svg",
+            userName:"smallcutecats",
+            status:"Segue você",
+        },
+    ]
+
     return (
         <div class="sugestoes">
             <div class="titulo">
                 Sugestões para você
                 <div>Ver tudo</div>
             </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/bad.vibes.memes.svg" />
-                    <div class="texto">
-                        <div class="nome">bad.vibes.memes</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/chibirdart.svg" />
-                    <div class="texto">
-                        <div class="nome">chibirdart</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/razoesparaacreditar.svg" />
-                    <div class="texto">
-                        <div class="nome">razoesparaacreditar</div>
-                        <div class="razao">Novo no Instagram</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/adorable_animals.svg" />
-                    <div class="texto">
-                        <div class="nome">adorable_animals</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/smallcutecats.svg" />
-                    <div class="texto">
-                        <div class="nome">smallcutecats</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
+        {sugestoes.map((sugestao => <Sugestao 
+        image={sugestao.image} 
+        userName={sugestao.userName}
+        status={sugestao.status} 
+        />))}
+                    
         </div>
     )
 }
