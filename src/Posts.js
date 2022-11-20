1984ead46b8c846ebc93cd4906f04446c5f06519
Ana Carvalho <anapaulacmoura@gmail.com>
2022-11-19 22:38:07 -0300

    adding map to Posts component

--- src/Posts.js
+++ src/Posts.js
@@ -1,77 +1,41 @@
+import Post from "./Post"
+
 export default function Posts(){
-    return(
+
+const posts = [
+    {userImage:"assets/img/meowed.svg",
+    userName: "meowed",
+    postImage:"assets/img/gato-telefone.svg",
+    likedImage:"assets/img/respondeai.svg" ,
+    likedBy: "respondeai",
+    likes:101.523,
+    },
+    {userImage:"assets/img/barked.svg",
+    userName: "barked",
+    postImage:"assets/img/dog.svg",
+    likedImage:"assets/img/adorable_animals.svg" ,
+    likedBy: "adorable_animals",
+    likes:99.159,
+    }
+
+]
+
+
+return (
         <div class="posts">
-                    <div class="post">
-                        <div class="topo">
-                            <div class="usuario">
-                                <img src="assets/img/meowed.svg" />
-                                meowed
-                            </div>
-                            <div class="acoes">
-                                <ion-icon name="ellipsis-horizontal"></ion-icon>
-                            </div>
-                        </div>
-
-                        <div class="conteudo">
-                            <img src="assets/img/gato-telefone.svg" />
-                        </div>
-
-                        <div class="fundo">
-                            <div class="acoes">
-                                <div>
-                                    <ion-icon name="heart-outline"></ion-icon>
-                                    <ion-icon name="chatbubble-outline"></ion-icon>
-                                    <ion-icon name="paper-plane-outline"></ion-icon>
-                                </div>
-                                <div>
-                                    <ion-icon name="bookmark-outline"></ion-icon>
-                                </div>
-                            </div>
-
-                            <div class="curtidas">
-                                <img src="assets/img/respondeai.svg" />
-                                <div class="texto">
-                                    Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="post">
-                        <div class="topo">
-                            <div class="usuario">
-                                <img src="assets/img/barked.svg" />
-                                barked
-                            </div>
-                            <div class="acoes">
-                                <ion-icon name="ellipsis-horizontal"></ion-icon>
-                            </div>
-                        </div>
-
-                        <div class="conteudo">
-                            <img src="assets/img/dog.svg" />
-                        </div>
-
-                        <div class="fundo">
-                            <div class="acoes">
-                                <div>
-                                    <ion-icon name="heart-outline"></ion-icon>
-                                    <ion-icon name="chatbubble-outline"></ion-icon>
-                                    <ion-icon name="paper-plane-outline"></ion-icon>
-                                </div>
-                                <div>
-                                    <ion-icon name="bookmark-outline"></ion-icon>
-                                </div>
-                            </div>
-
-                            <div class="curtidas">
-                                <img src="assets/img/adorable_animals.svg" />
-                                <div class="texto">
-                                    Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            {posts.map((post => <Post 
+            userImage={post.userImage} 
+            userName={post.userName}
+            postImage={post.postImage}
+            likedImage={post.likedImage}
+            likedBy={post.likedBy}
+            likes={post.likes}          
+            
+            
+            />))} 
+               
                 </div>
     )
 }
+
+
